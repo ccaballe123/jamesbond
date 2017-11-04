bf382bb7191a3158eaaee9475bfa84b00b45cd8c
michael-langaman <langaman.michael@gmail.com>
2017-11-04 16:19:33 -0400

    game/player class update

---
+++ src/game/backend/Player.java
@@ -0,0 +1,16 @@
+package src.game;
+
+import game.backend.*;
+import java.util.Collections;
+
+public class Player {
+	
+	private ArrayList<ArrayList<Card>> _hand;
+	private Game _game;
+
+	public Player(Game game) {
+		_game = game;
+		_hands = null;
+	}
+
+}
