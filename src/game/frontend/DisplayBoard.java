8285e99084628968b78455b55703132828f30b88
ccaballe <ccaballe@buffalo.edu>
2017-11-04 19:15:31 -0400

    displayboard/move driver

--- src/game/frontend/DisplayBoard.java
+++ src/game/frontend/DisplayBoard.java
@@ -2,6 +2,26 @@ package game.frontend;
 
 import game.backend.*;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class DisplayBoard {
-	
+
+	private JFrame board;
+
+	private JPanel p1;
+	private JPanel p2;
+	private JPanel midCards;
+
+	public DisplayBoard(){
+		
+	}
+
+	private void gui(){
+		board = new JFrame();
+		board.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		board.setUndecorated(true);
+		board.setVisible(true);
+	}
+
 }
