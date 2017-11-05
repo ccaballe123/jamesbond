1af2b36ade7602f18982893be6d695385342f82f
Phdmani <jmaniara@buffalo.edu>
2017-11-04 21:47:13 -0400

    guih

--- src/game/frontend/DisplayBoard.java
+++ src/game/frontend/DisplayBoard.java
@@ -7,9 +7,11 @@ import java.awt.Color;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-@SupressWarnings("serial")
+@SuppressWarnings("serial")
 public class DisplayBoard extends JFrame {
 
 	private JFrame board;
@@ -38,12 +40,14 @@ public class DisplayBoard extends JFrame {
 		board.add(p1, BorderLayout.NORTH);
 		board.add(p2, BorderLayout.SOUTH);
 		
-		infoPanel = new JFrame();
+		infoPanel = new JFrame("Instructions");
 		infoPanel.setBackground(Color.YELLOW);
 		infoPanel.setPreferredSize(new Dimension(700, 700));
     	infoPanel.setVisible(true);
 		infoPanel.pack();
 		infoPanel.setLocationRelativeTo(null);
+		JLabel tf = new JLabel("<html>"+"Secret Agent 007 your mission is completeing this game alive.In order to be the best that ever was, you are given sets of cards and must find every suite of one card in each set.You can pick cards from 4 cards in the middle but must replace it with a card from one of your piles.You can only peek at each of your sets."+"<html/>");
+		infoPanel.add(tf);
 
 
 
