a01cdee35c1e152d4120860d3ca72e2f1386f7ad
ccaballe <ccaballe@buffalo.edu>
2017-11-04 22:26:58 -0400

    jlabel arrange fix

--- src/game/frontend/DisplayBoard.java
+++ src/game/frontend/DisplayBoard.java
@@ -9,7 +9,6 @@ import java.awt.Dimension;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 @SuppressWarnings("serial")
 public class DisplayBoard extends JFrame {
@@ -36,6 +35,8 @@ public class DisplayBoard extends JFrame {
 		p2 = new JPanel();
 		p1.setBackground(Color.RED);
 		p2.setBackground(Color.BLUE);
+		p1.setPreferredSize(new Dimension(300, 300));
+		p2.setPreferredSize(new Dimension(300, 300));
 		board.add(p1, BorderLayout.NORTH);
 		board.add(p2, BorderLayout.SOUTH);
 		
@@ -45,11 +46,20 @@ public class DisplayBoard extends JFrame {
 		infoPanel.pack();
 		infoPanel.setLocationRelativeTo(null);
 		
+		JPanel labelTf = new JPanel();
+		JPanel labelKeyM = new JPanel();
 		JLabel tf = new JLabel("<html>"+"Secret Agent 007 your mission is completeing this game alive.In order to be the best that ever was, you are given sets of cards and must find every suite of one card in each set.You can pick cards from 4 cards in the middle but must replace it with a card from one of your piles.You can only peek at each of your sets."+"<html/>");
 		JLabel keyMovements = new JLabel("<html>" + "The top portion of the screen will contain Player 1s hands, while the bottom sportion will hold Player 2s hands. Player 1 cycles through their sets with keys 'A' and 'D'(Player 2 uses Left and Right). In order to cycle through the cards in a set Player 1 uses 'W' and 'S'(Player 2 uses Up and Down). In order to select a card, Player 1 uses left-CTRL (Player 2 uses BACKSPACE)." + "html");
 		
-		infoPanel.add(tf);
-		infoPanel.add(keyMovements);
+		labelTf.add(tf);
+		labelKeyM.add(keyMovements);
+		labelTf.setPreferredSize(new Dimension(600, 200));
+		labelKeyM.setPreferredSize(new Dimension(600, 200));
+
+
+		infoPanel.add(labelTf, BorderLayout.NORTH);
+		infoPanel.add(labelKeyM, BorderLayout.SOUTH);
+
 		infoPanel.setVisible(true);
 
 
