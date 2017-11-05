6b79736016332f7d5e588c351fc775dac6da4f69
ccaballe <ccaballe@buffalo.edu>
2017-11-04 20:07:23 -0400

    driver test

---
+++ src/game/Driver.java
@@ -0,0 +1,13 @@
+package game;
+
+import game.backend.*;
+import game.frontend.*;
+
+public class Driver {
+
+	public static void main(String[] args){
+		DisplayBoard test = new DisplayBoard();
+		test.gui();
+	}
+
+}
