ce9e6b0014e658b5d83472c63253908e98b55ee5
Phdmani <jmaniara@buffalo.edu>
2017-11-04 20:16:18 -0400

    driver

--- src/game/frontend/DisplayBoard.java
+++ src/game/frontend/DisplayBoard.java
@@ -5,7 +5,7 @@ import game.backend.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class DisplayBoard {
+public class DisplayBoard extends JFrame {
 
 	private JFrame board;
 
@@ -14,10 +14,12 @@ public class DisplayBoard {
 	private JPanel midCards;
 
 	public DisplayBoard(){
+		super("James Bond Card Game");
+		gui();
 		
 	}
 
-	private void gui(){
+	 public void gui(){
 		board = new JFrame();
 		board.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		board.setUndecorated(true);
