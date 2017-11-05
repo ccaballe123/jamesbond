4630f89cd56a705bb5db2ffd81a4ef7fc2a94fca
Phdmani <jmaniara@buffalo.edu>
2017-11-04 22:12:07 -0400

    his

--- src/game/frontend/DisplayBoard.java
+++ src/game/frontend/DisplayBoard.java
@@ -22,6 +22,7 @@ public class DisplayBoard extends JFrame {
 	private JPanel midCards;
 
 	public DisplayBoard(){
+		
 	}
 
 	public void gui(){
@@ -39,14 +40,14 @@ public class DisplayBoard extends JFrame {
 		board.add(p2, BorderLayout.SOUTH);
 		
 		infoPanel = new JFrame("Instructions");
-		infoPanel.setBackground(Color.YELLOW);
+		infoPanel.getContentPane().setBackground(Color.YELLOW);
 		infoPanel.setPreferredSize(new Dimension(700, 700));
 		infoPanel.pack();
 		infoPanel.setLocationRelativeTo(null);
 		
 		JLabel tf = new JLabel("<html>"+"Secret Agent 007 your mission is completeing this game alive.In order to be the best that ever was, you are given sets of cards and must find every suite of one card in each set.You can pick cards from 4 cards in the middle but must replace it with a card from one of your piles.You can only peek at each of your sets."+"<html/>");
 		JLabel keyMovements = new JLabel("<html>" + "The top portion of the screen will contain Player 1s hands, while the bottom sportion will hold Player 2s hands. Player 1 cycles through their sets with keys 'A' and 'D'(Player 2 uses Left and Right). In order to cycle through the cards in a set Player 1 uses 'W' and 'S'(Player 2 uses Up and Down). In order to select a card, Player 1 uses left-CTRL (Player 2 uses BACKSPACE)." + "html");
-
+		
 		infoPanel.add(tf);
 		infoPanel.add(keyMovements);
 		infoPanel.setVisible(true);
