7ae48a578a128b1d7b9dc6579d66c9a5f94a41aa
Phdmani <jmaniara@buffalo.edu>
2017-11-04 20:18:06 -0400

    drive

---
+++ src/game/backend/Driver.java
@@ -0,0 +1,11 @@
+package game.backend;
+import game.frontend.DisplayBoard;
+
+public class Driver {
+	public static void main(String args[]){
+//		Game myGame = new Game(0);
+		DisplayBoard myD = new DisplayBoard();
+		myD.gui();
+		
+	}
+}
